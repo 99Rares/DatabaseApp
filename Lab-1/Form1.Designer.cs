0aa162ec6f59553fc804423738dc3bdbef8fa5c7
Rareș Dan <46723989+99Rares@users.noreply.github.com>
2021-05-20 15:54:35 +0300

    Add project files.

---
+++ Lab-1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.actorData = new System.Windows.Forms.DataGridView();
+            this.awardData = new System.Windows.Forms.DataGridView();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.actorData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.awardData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // actorData
+            // 
+            this.actorData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.actorData.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.actorData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.actorData.Location = new System.Drawing.Point(12, 48);
+            this.actorData.Name = "actorData";
+            this.actorData.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.actorData.Size = new System.Drawing.Size(481, 183);
+            this.actorData.TabIndex = 0;
+            this.actorData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.actorData_CellClick);
+            // 
+            // awardData
+            // 
+            this.awardData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.awardData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.awardData.Location = new System.Drawing.Point(499, 48);
+            this.awardData.Name = "awardData";
+            this.awardData.Size = new System.Drawing.Size(500, 183);
+            this.awardData.TabIndex = 1;
+            this.awardData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.awardData_CellClick);
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(197, 301);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 12;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(197, 348);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 13;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(197, 395);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 14;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(373, 247);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 15;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 469);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.awardData);
+            this.Controls.Add(this.actorData);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SGBD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.actorData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.awardData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView actorData;
+        private System.Windows.Forms.DataGridView awardData;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ Lab-1/Form1.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab_1
+{
+    public partial class Form1 : Form
+    {
+        //private readonly string connectionString = "Data Source=RARES-DAN-ASUS\\SQLEXPRESS;Initial Catalog=VideoRental;" + "Integrated Security=true";
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+        private readonly string vaterTabelle = ConfigurationManager.AppSettings["vaterTable"];
+        private readonly string kindTabelle = ConfigurationManager.AppSettings["kindTable"];
+        //private readonly string actorQuerry = "select * from Actor";
+        //private readonly string awardQuerry = "select * from ActorAward";
+
+        private DataRelation relation;
+        private BindingSource bindVater;
+        private BindingSource bindKind;
+        private DataSet dataSet;
+
+        List<TextBox> tb = new List<TextBox>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addActorTable()
+        {
+            string query = "SELECT * FROM [" + vaterTabelle + "]";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connectionString);
+            dataAdapter.Fill(dataSet, vaterTabelle);
+        }
+
+        private void addActorAwardTable()
+        {
+
+            string query = "select * from [" + kindTabelle + "]";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connectionString);
+            dataAdapter.Fill(dataSet, kindTabelle);
+        }
+        private void addRelation()
+        {
+            var primaryKey = findeSchlussel(vaterTabelle, "primary");
+            var foreignKey = findeSchlussel(kindTabelle, "foreign");
+
+            relation = new DataRelation("FK_" + vaterTabelle + "_" + kindTabelle, dataSet.Tables[vaterTabelle].Columns[primaryKey], dataSet.Tables[kindTabelle].Columns[foreignKey]);
+            dataSet.Relations.Add(relation);
+
+            bindVater = new BindingSource
+            {
+                DataSource = dataSet,
+                DataMember = vaterTabelle
+            };
+            bindKind = new BindingSource
+            {
+                DataSource = bindVater,
+                DataMember = "FK_" + vaterTabelle + "_" + kindTabelle
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            dataSet = new DataSet();
+            insertButton.Enabled = false;
+            deleteButton.Enabled = false;
+            UpdateButton.Enabled = false;
+            try
+            {
+                connection.Open();
+                addActorTable();
+                addActorAwardTable();
+                addRelation();
+
+
+                connection.Close();
+                actorData.DataSource = bindVater;
+                textBox();
+                Labels();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Console.WriteLine(ex.Message);
+                connection.Close();
+            }
+        }
+
+        private void actorData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            awardData.DataSource = bindKind;
+            emptyBoxes();
+
+        }
+
+        private void awardData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            fillBoxes();
+
+            insertButton.Enabled = true;
+            deleteButton.Enabled = true;
+            UpdateButton.Enabled = true;
+        }
+        private void fillBoxes()
+        {
+            emptyBoxes();
+            var selectedRow = this.awardData.CurrentRow;
+
+            for (int i = 0; i < tb.Count; i++)
+            {
+                tb[i].Text = selectedRow.Cells[i].Value.ToString();
+            }
+        }
+        private void emptyBoxes()
+        {
+            for (int i = 0; i < tb.Count; i++)
+            {
+                tb[i].Text = "";
+            }
+        }
+        private void textBox()
+        {
+            int distance = 0;
+            for (int i = 0; i < findColumnNumber(kindTabelle); i++)
+            {
+                TextBox textBox = new TextBox
+                {
+                    Location = new Point(360, 300 + distance),
+                    Width = 200,
+                    Enabled = true
+                };
+                this.Controls.Add(textBox);
+
+                tb.Add(textBox);
+                distance += 33;
+            }
+        }
+        private void Labels()
+        {
+            List<string> columns = findColumnNames(kindTabelle);
+            int distance = 0;
+            for (int i = 0; i < findColumnNumber(kindTabelle); i++)
+            {
+                Label label = new Label
+                {
+                    Location = new Point(260, 300 + distance),
+                    Text = columns[i],
+                    TextAlign = ContentAlignment.MiddleRight
+                };
+                this.Controls.Add(label);
+
+                distance += 33;
+            }
+        }
+        private string ColumnsToInsert()
+        {
+            string columns = "";
+            for (int i = 0; i < tb.Count; i++)
+            {
+                if (i != tb.Count - 1)
+                    if (dataSet.Tables[kindTabelle].Columns[i].DataType.ToString() == "System.DateTime")
+                        columns = columns + "CONVERT(DATE, @textBox" + i.ToString() + ", 105),  ";
+                    else
+                        columns = columns + "@textBox" + i.ToString() + ", ";
+                else
+                    if (dataSet.Tables[kindTabelle].Columns[i].DataType.ToString() == "System.DateTime")
+                    columns = columns + "CONVERT(DATE, @textBox" + i.ToString() + ", 105) ";
+                else
+                    columns = columns + "@textBox" + i.ToString();
+            }
+
+            return columns;
+        }
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            string columns = ColumnsToInsert();
+            string insertQ = "insert into " + kindTabelle + " values (" + columns + ")";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand command = new SqlCommand(insertQ, connection);
+            for (int i = 0; i < tb.Count; i++)
+            {
+                command.Parameters.AddWithValue("textBox" + i.ToString(), tb[i].Text);
+            }
+
+            //string id = idtext.Text;
+
+            //sqlDataAdapter.InsertCommand = new SqlCommand(insertQ, connection);
+            //sqlDataAdapter.InsertCommand.Parameters.Add("@ID", SqlDbType.VarChar, 50);
+            //sqlDataAdapter.InsertCommand.Parameters["@ID"].Value = id;
+
+            //sqlDataAdapter.InsertCommand.Parameters.Add("@Name", SqlDbType.VarChar, 50);
+            //sqlDataAdapter.InsertCommand.Parameters["@Name"].Value = nametext.Text;
+
+            //sqlDataAdapter.InsertCommand.Parameters.Add("@ActorID", SqlDbType.Int);
+            //sqlDataAdapter.InsertCommand.Parameters["@ActorID"].Value = actorIDtext.Text;
+
+            //sqlDataAdapter.InsertCommand.Parameters.Add("@Category", SqlDbType.VarChar, 50);
+            //sqlDataAdapter.InsertCommand.Parameters["@Category"].Value = categorytext.Text;
+
+            //sqlDataAdapter.InsertCommand.Parameters.Add("@Date", SqlDbType.Date);
+            //sqlDataAdapter.InsertCommand.Parameters["@Date"].Value = dateText.Text;
+
+            try
+            {
+                connection.Open();
+                var rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("New Row inserted");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+                //MessageBox.Show("Row NOT inserted");
+                MessageBox.Show("Row NOT inserted", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+
+                dataSet.Tables[kindTabelle].Clear();
+                addActorAwardTable();
+            }
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            var selectedRow = this.awardData.CurrentRow;
+            var primaryKey = findeSchlussel(kindTabelle, "primary");
+            var PK = selectedRow.Cells[primaryKey].Value;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            //sqlDataAdapter.DeleteCommand.Parameters["@ID"].Value = id.ToString();
+            SqlCommand command = new SqlCommand("DELETE FROM " + kindTabelle + " WHERE " + primaryKey + " = @PK", connection);
+            command.Parameters.AddWithValue("@PK", PK);
+
+            try
+            {
+                connection.Open();
+                var rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Row deleted");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+                //MessageBox.Show("Row NOT deleted");
+                MessageBox.Show("Row NOT deleted", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                connection.Close();
+
+                dataSet.Tables[kindTabelle].Clear();
+                addActorAwardTable();
+            }
+
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+
+            var selectedRow = this.awardData.CurrentRow;
+            var primaryKey = findeSchlussel(kindTabelle, "primary");
+            var PK = selectedRow.Cells[primaryKey].Value;
+
+            string columns = ColumnsToUpdate();
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("UPDATE " + kindTabelle + " SET " + columns + " WHERE " + primaryKey + " = @PK", connection);
+            for (int i = 0; i < tb.Count; i++)
+            {
+                command.Parameters.AddWithValue("textBox" + i.ToString(), tb[i].Text);
+            }
+            command.Parameters.AddWithValue("@PK", PK);
+            //sqlDataAdapter.UpdateCommand.Parameters["@IDog"].Value = id.ToString();
+
+            //sqlDataAdapter.UpdateCommand.Parameters.Add("@ID", SqlDbType.VarChar, 50);
+            //sqlDataAdapter.UpdateCommand.Parameters["@ID"].Value = idtext.Text;
+
+            //sqlDataAdapter.UpdateCommand.Parameters.Add("@Name", SqlDbType.VarChar, 50);
+            //sqlDataAdapter.UpdateCommand.Parameters["@Name"].Value = nametext.Text;
+
+            //sqlDataAdapter.UpdateCommand.Parameters.Add("@ActorID", SqlDbType.Int);
+            //sqlDataAdapter.UpdateCommand.Parameters["@ActorID"].Value = actorIDtext.Text;
+
+            //sqlDataAdapter.UpdateCommand.Parameters.Add("@Category", SqlDbType.VarChar, 50);
+            //sqlDataAdapter.UpdateCommand.Parameters["@Category"].Value = categorytext.Text;
+
+            //sqlDataAdapter.UpdateCommand.Parameters.Add("@Date", SqlDbType.Date);
+            //sqlDataAdapter.UpdateCommand.Parameters["@Date"].Value = dateText.Text;
+
+            try
+            {
+                connection.Open();
+                var rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Row updated");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+                //Console.ReadLine();
+                //MessageBox.Show("Row NOT updated");
+                MessageBox.Show("Row NOT updated", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+              
+            }
+            finally
+            {
+                connection.Close();
+
+                dataSet.Tables[kindTabelle].Clear();
+                addActorAwardTable();
+            }
+        }
+        private string ColumnsToUpdate()
+        {
+            string columns = "";
+            for (int i = 0; i < tb.Count; i++)
+            {
+                if (i != tb.Count - 1)
+                    if (dataSet.Tables[kindTabelle].Columns[i].DataType.ToString() == "System.DateTime")
+                        columns = columns + dataSet.Tables[kindTabelle].Columns[i].ColumnName.ToString() + " = CONVERT(DATE, @textBox" + i.ToString() + ", 105), ";
+                    else
+                    {
+                        columns = columns + dataSet.Tables[kindTabelle].Columns[i].ColumnName.ToString() + " = @textBox" + i.ToString() + ", ";
+                        //MessageBox.Show(dataSet.Tables[kindTable].Columns[i].DataType.ToString());
+                    }
+                else
+                    if (dataSet.Tables[kindTabelle].Columns[i].DataType.ToString() == "System.DateTime")
+                    columns = columns + dataSet.Tables[kindTabelle].Columns[i].ColumnName.ToString() + " = CONVERT(DATE, @textBox" + i.ToString() + ", 105) ";
+                else
+                {
+                    columns = columns + dataSet.Tables[kindTabelle].Columns[i].ColumnName.ToString() + " = @textBox" + i.ToString();
+                    //MessageBox.Show(dataSet.Tables[kindTable].Columns[i].DataType.ToString());
+                }
+            }
+
+            return columns;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tb.Count; i++)
+            {
+                tb[i].Text = "";
+            }
+        }
+        private string findeSchlussel(string tableName, string keyType)
+        {
+            string keyName = "";
+            string query = "";
+
+            if (keyType == "primary")
+            {
+                query = "select c.column_name " +
+                        "from information_schema.table_constraints t " +
+                        "join information_schema.constraint_column_usage c " +
+                        "on c.constraint_name = t.constraint_name " +
+                        "where c.table_name = '" + tableName + "' and t.constraint_type = '" + keyType + " key'";
+
+            }
+            if (keyType == "foreign")
+            {
+                query = "select k.column_name " +
+                        "from information_schema.key_column_usage k " +
+                        "inner join information_schema.referential_constraints r " +
+                        "on k.constraint_name = r.constraint_name " +
+                        "inner join information_schema.constraint_column_usage c " +
+                        "on c.constraint_name = r.unique_constraint_name " +
+                        "where k.table_name = '" + kindTabelle + "' and c.table_name = '" + vaterTabelle + "'";
+
+            }
+
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    keyName = (string)reader[0];
+                }
+            }
+            sqlConnection.Close();
+
+            return keyName;
+        }
+        private int findColumnNumber(string name)
+        {
+            return dataSet.Tables[name].Columns.Count;
+        }
+        private List<string> findColumnNames(string tableName)
+        {
+            List<string> columns = new List<string>();
+
+            for (int i = 0; i < findColumnNumber(tableName); i++)
+            {
+                columns.Add(dataSet.Tables[tableName].Columns[i].ColumnName);
+            }
+            return columns;
+        }
+    }
+}
